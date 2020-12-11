5e06f034b5db7f7c8d3bf5978c5f4be6d5bb6162
hamzashahab1610 <hamzashahab1610@gmail.com>
2020-12-11 19:22:38 +0500

    frontend init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import Table from "./components/Table";
+
+function App() {
+	return (
+		<div className="App">
+			<h1>Tables App</h1>
+			<Table name={"Market Table"} />
+		</div>
+	);
+}
+
+export default App;
